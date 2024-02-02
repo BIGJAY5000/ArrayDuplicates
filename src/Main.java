37f7b97d98fc7ec276f1b2e9c9fcfadb221d7f29
BIGJAY5000 <144457717+BIGJAY5000@users.noreply.github.com>
2024-02-02 07:57:50 -0500

    heyyyboooo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random rand = new Random();
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[10];
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(10) + 1;
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("");
+        for (int j = 0; j < array.length; j++) {
+            int current = array[j];
+            count = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (current == array[i]) {
+                    count++;
+                }
+            }
+            insert(array, current);
+            if (count > 1 && current != 0) {
+                System.out.println(current + " occurs " + count + " times in the array");
+            }
+        }
     }
+    public static int [] insert (int [] arr, int element) {
+        int [] nums = arr;
+        for (int i = 0; i < nums.length; i++) {
+            if (arr[i] == element) {
+                arr[i] = 0;
+            }
+        }
+        return nums;
+    }
+
 }
